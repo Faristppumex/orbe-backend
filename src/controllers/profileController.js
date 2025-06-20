cef206b560f9c661d6a23c2c8d0091945361d5e5
Faristp <farismuhammed101@gmail.com>
2025-06-20 15:01:32 +0530

    no mentions

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -1,7 +1,6 @@
 const { fetchProfile } = require("../services/profileService");
 
 async function getProfile(req, res) {
-  console.log("Fetching profile data...");
   try {
     const symbol = req.query.symbol || "AAPL"; // Get symbol from query, default to AAPL
     const data = await fetchProfile(symbol); // Pass symbol to service
